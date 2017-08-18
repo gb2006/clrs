2202b0d36132ad74dbc77e3ad325fdbcc7767688
gb2006 <gb200622@gmail.com>
2017-08-17 23:47:33 -0700

    Add project and selection sort

---
+++ sorting/AssortedAlgos/AssortedAlgos/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssortedAlgos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = GetTestArr();
+
+            new SelectionSort().Sort(arr);
+            Print(arr);
+        }
+
+        static int[] GetTestArr()
+        {
+            Random r = new Random();
+            int size = r.Next(10, 1000);
+
+            int[] arr = new int[size];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = r.Next(10000);
+            }
+
+            return arr;
+        }
+
+        static void Print(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write("{0} ", arr[i]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
