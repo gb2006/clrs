a99271f361d019752851a3a541348ed5a674b445
gb2006 <gb200622@gmail.com>
2017-08-18 01:28:21 -0700

    Fix unused usings

--- sorting/AssortedAlgos/AssortedAlgos/MergeSort.cs
+++ sorting/AssortedAlgos/AssortedAlgos/MergeSort.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AssortedAlgos
+﻿namespace AssortedAlgos
 {
     class MergeSort
     {
