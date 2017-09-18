0b77524a2195f510abaa12f219170da65f2b0730
gb2006 <gb200622@gmail.com>
2017-09-17 20:02:23 -0700

    Add generate permutations

---
+++ Recursion/GeneratePermutations/GeneratePermutations/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace GeneratePermutations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] arr = { 'a', 'b', 'c', 'd' };
+            GeneratePermutations(arr);            
+        }
+
+        static void GeneratePermutations(char[] arr)
+        {
+            GeneratePermutations(arr, 0, arr.Length - 1);
+        }
+
+        static void GeneratePermutations(char[] arr, int st, int en)
+        {
+            if (st > en)
+            {
+                Console.WriteLine(arr);
+                return;
+            }
+
+            for (int i = st; i <= en; i++)
+            {
+                Swap(arr, i, st);
+                GeneratePermutations(arr, st + 1, en);
+                Swap(arr, i, st);
+            }
+        }
+
+        static void Swap(char[] arr, int p1, int p2)
+        {
+            char temp = arr[p1];
+            arr[p1] = arr[p2];
+            arr[p2] = temp;
+        }
+    }
+}
