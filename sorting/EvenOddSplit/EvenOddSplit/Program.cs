8021cd5332dc3270ba211c7d55196a59e3cc424c
gb2006 <gb200622@gmail.com>
2017-09-24 02:12:40 -0700

    Add even/odd split

---
+++ sorting/EvenOddSplit/EvenOddSplit/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace EvenOddSplit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static int[] groupNumbers(int[] arr)
+        {
+            if (arr == null || arr.Length <= 1)
+            {
+                return arr;
+            }
+
+            int marker = 0;
+            for (int k = 0; k < arr.Length; k++)
+            {
+                if (arr[k] % 2 == 0)
+                {
+                    int temp = arr[marker];
+                    arr[marker] = arr[k];
+                    arr[k] = temp;
+                    marker++;
+                }
+            }
+            return arr;
+        }
+    }
+}
